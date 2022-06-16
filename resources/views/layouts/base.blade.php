3c36dec33de0a56f646e7f0e36dfef09d59f0e54
happynet <49040138+happynet78@users.noreply.github.com>
2022-06-16 17:18:37 +0900

    Update base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Home</title>	
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/favicon.ico') }}">
+	<link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/favicon.ico') }}">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/animate.css') }}">
@@ -23,12 +23,12 @@
 <body class="home-page home-01 ">
 
 	<!-- mobile menu -->
-    <div class="mercado-clone-wrap">
-        <div class="mercado-panels-actions-wrap">
-            <a class="mercado-close-btn mercado-close-panels" href="#">x</a>
-        </div>
-        <div class="mercado-panels"></div>
-    </div>
+	<div class="mercado-clone-wrap">
+		<div class="mercado-panels-actions-wrap">
+			<a class="mercado-close-btn mercado-close-panels" href="#">x</a>
+		</div>
+		<div class="mercado-panels"></div>
+	</div>
 
 	<!--header-->
 	<header id="header" class="header header-style-1">
